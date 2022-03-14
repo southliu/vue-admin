d96db55b1feed6357806f3dbba52aebc49fbd8c2
“Southliu” <1275093225@qq.com>
2022-03-14 18:03:45 +0800

    wip: 标签页功能

--- src/layouts/components/Tabs.vue
+++ src/layouts/components/Tabs.vue
@@ -2,16 +2,47 @@
   <Tabs
     v-model:activeKey="activeKey"
     hide-add
-    type="editable-card"
     @change="onChange"
-    @edit="onEdit"
   >
     <TabPane
+      class="overflow-auto"
       v-for="item in tabs"
       :key="item.key"
-      :tab="item.title"
       :closable="item.closable"
-    />
+    >
+      <template #tab>
+        <Dropdown :trigger="['contextmenu']">
+          <div class="flex w-full p-2 -mt-5px border">
+            <div class="mr-2">{{ item.title }}</div>
+            <div @click="handleRemove(item.key)">X</div>
+          </div>
+          <template #overlay>
+            <Menu>
+              <MenuItem key="1">
+                <RedoOutlined class="mr-1 transform rotate-270" />
+                <span>重新加载</span>
+              </MenuItem>
+              <MenuItem key="2">
+                <CloseOutlined class="mr-1" />
+                <span>关闭标签页</span>
+              </MenuItem>
+              <MenuItem key="3">
+                <VerticalAlignMiddleOutlined class="mr-1 transform rotate-90" />
+                <span>关闭其他</span>
+              </MenuItem>
+              <MenuItem key="4">
+                <VerticalAlignTopOutlined class="mr-1 transform rotate-270" />
+                <span>关闭左侧</span>
+              </MenuItem>
+              <MenuItem key="5">
+                <VerticalAlignTopOutlined class="mr-1 transform rotate-90" />
+                <span>关闭右侧</span>
+              </MenuItem>
+            </Menu>
+          </template>
+        </Dropdown>
+      </template>
+    </TabPane>
   </Tabs>
 </template>
 
@@ -19,12 +50,36 @@
 import { defineComponent } from 'vue'
 import { useTabStore } from '@/stores/tabs'
 import { storeToRefs } from 'pinia'
-import { Tabs, TabPane } from 'ant-design-vue'
+import {
+  Tabs,
+  TabPane,
+  Menu,
+  MenuItem,
+  Dropdown,
+  Button
+} from 'ant-design-vue'
+import {
+  RedoOutlined,
+  CloseOutlined,
+  VerticalAlignTopOutlined,
+  VerticalAlignMiddleOutlined
+} from '@ant-design/icons-vue'
 import { useRouter } from 'vue-router'
 import type { Key } from 'ant-design-vue/lib/_util/type'
 
 export default defineComponent({
-  components: { Tabs, TabPane },
+  components: {
+    RedoOutlined,
+    CloseOutlined,
+    VerticalAlignTopOutlined,
+    VerticalAlignMiddleOutlined,
+    Tabs,
+    TabPane,
+    Menu,
+    MenuItem,
+    Dropdown,
+    Button
+  },
   setup() {
     const router = useRouter()
     const tabStore = useTabStore()
@@ -36,20 +91,14 @@ export default defineComponent({
     }
     
     // 移除当前标签页
-    const remove = (targetKey: string) => {
+    const handleRemove = (targetKey: string) => {
       tabStore.removeTabs(targetKey)
-    };
-
-    // 编辑
-    const onEdit = (targetKey: string | MouseEvent | Key | KeyboardEvent) => {
-      console.log('onEdit:', targetKey)
-      remove(targetKey as string);
     };
 
     return {
       tabs,
       activeKey,
-      onEdit,
+      handleRemove,
       onChange
     };
   }
