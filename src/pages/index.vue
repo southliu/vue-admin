e790425a28aa4bf4bc7c87575ad307d2fc586325
southliu <1275093225@qq.com>
2023-05-15 23:14:24 +0800

    refactor: 首页跳转第一个有效页面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="flex justify-center">
-    <img
-      :src="Image"
-      alt="Image"
-      class="w-980px h-full container"
-    >
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts" setup>
-import Image from '@/assets/images/welcome.png'
+import { onMounted } from 'vue'
 import { useToken } from '@/hooks/useToken'
+import { defaultMenus } from '@/menus'
+import { getFirstMenu } from '@/menus/utils/helper'
+import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 
 const { getToken } = useToken()
+const userStore = useUserStore()
+const { permissions } = storeToRefs(userStore)
 const router = useRouter()
 const token = getToken()
-if (!token) router.push('/login')
+
+onMounted(() => {
+  if (!token) return router.push('/login')
+
+  // 跳转第一个有效的菜单路径
+  goFirstMenu()
+})
+
+// 跳转第一个有效的菜单路径
+const goFirstMenu = () => {
+  const firstMenu = getFirstMenu(defaultMenus, permissions.value)
+  router.push(firstMenu)
+}
 </script>
