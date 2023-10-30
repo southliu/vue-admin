0d1824979a65a9fb6dda5fb2f70866f1ebb93280
southliu <1275093225@qq.com>
2023-10-30 21:37:13 +0800

    fix: 打包白屏问题

--- build/utils/helper.ts
+++ build/utils/helper.ts
@@ -1,5 +1,3 @@
-import { PAGES_PATH, PAGE_PREFIX } from './config';
-
 type EnvConfigs = Record<string, string>
 
 // env数据
@@ -37,47 +35,10 @@ export function splitJSModules(id: string) {
   const pnpmName = id.includes('.pnpm') ? '.pnpm/' : '';
   const fileName = `node_modules/${pnpmName}`;
 
-  let result = id
+  const result = id
     .split(fileName)[1]
-    .split('/')[0];
-
-  if (result.includes('@')) {
-    const first = result.indexOf('@');
-    if (first > 0) {
-      result = result.substring(0, first);
-    } else {
-      const second = result.indexOf('@', 1);
-      result = result.substring(0, second);
-    }
-  }
+    .split('/')[0]
+    .toString();
 
   return result;
-}
-
-/**
- * 页面分包处理
- * @param id - 标识符
- */
-export function splitPage(id: string) {
-  const fileName = PAGES_PATH;
-  const file = id.split(fileName)[1];
-  const categorize = file?.split('/')?.[0] || '';
-  let result = file?.split('/')?.[1] || 'index';
-
-  if (result.includes('/')) result = result?.split('/')[0] || '';
-  if (result.includes('.tsx')) result = result.substring(0, result.length - 4);
-
-  // 组件
-  if (result === 'components' || result === 'component') {
-    let compName = '/components/';
-    if (id.includes('/component/')) compName = '/component/';
-
-    let comResult = id.split(compName)[1];
-    if (comResult.includes('/')) comResult = comResult?.split('/')[0] || '';
-    if (comResult.includes('.tsx')) comResult = comResult.substring(0, comResult.length - 4);
-
-    return `${PAGE_PREFIX}${categorize}_comp_${comResult}`;
-  }
-
-  return `${PAGE_PREFIX}${categorize}_${result}`;
 }
