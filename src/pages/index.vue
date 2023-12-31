382171858e513f4d7fcbdc008f06c998e4e985dd
southliu <1275093225@qq.com>
2023-12-31 14:38:29 +0800

    fix: 首页不显示菜单

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,23 +7,29 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
 import { useToken } from '@/hooks/useToken';
-import { handleFilterApiMenu, getFirstMenu } from '@/utils/menu';
+import { handleFilterApiMenu, getFirstMenu, getMenuByKey } from '@/utils/menu';
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
-import { useMenuStore } from '@/stores/menu';
 import { Spin } from 'ant-design-vue';
+import { useTabStore } from '@/stores/tabs';
+import { useMenuStore } from '@/stores/menu';
 import { getPermissions } from '@/servers/permission';
 import { permissionsToArray } from '@/utils/permissions';
 import { getSystemMenuTree } from '@/servers/system/menu';
 
 const { getToken } = useToken();
+const tabStore = useTabStore();
 const userStore = useUserStore();
 const menuStore = useMenuStore();
 const { menuList } = storeToRefs(menuStore);
 const { permissions } = storeToRefs(userStore);
-const { setMenus } = menuStore;
+const { setMenus, setSideMenu, setTopMenuKey } = menuStore;
 const { setUserInfo, setPermissions } = userStore;
+const {
+  setActiveKey,
+  addTabs,
+} = tabStore;
 const router = useRouter();
 const token = getToken();
 
@@ -46,7 +52,6 @@ const getUserInfo = async () => {
 
     setUserInfo(user);
     setPermissions(newPermissions);
-    getUserMenu(newPermissions);
     return newPermissions;
   } catch(err) {
     console.error(err);
@@ -67,6 +72,21 @@ const getUserMenu = async (permissions: string[]) => {
   }
 };
 
+/** 获取路径相匹配的菜单 */
+const getMatchMenu = (path: string) => {
+  for (let i = 0; i < menuList.value?.length; i++) {
+    const item = menuList.value[i];
+    if (path.includes(item.key)) {
+      return {
+        path: item.key,
+        list: item.children || [],
+      };
+    }
+  }
+
+  return {};
+};
+
 // 跳转第一个有效的菜单路径
 const goFirstMenu = async () => {
   let currentMenuList = menuList.value;
@@ -81,5 +101,21 @@ const goFirstMenu = async () => {
 
   const firstMenu = getFirstMenu(currentMenuList, currentPermissions);
   router.push(firstMenu);
+  const menuByKeyProps = {
+    menus: currentMenuList,
+    permissions: currentPermissions,
+    key: firstMenu
+  };
+  const newItems = getMenuByKey(menuByKeyProps);
+  if (newItems) {
+    setActiveKey(newItems.key);
+    addTabs(newItems);
+  }
+
+  const { path, list } = getMatchMenu(firstMenu);
+  if (path) {
+    setTopMenuKey(path);
+    setSideMenu(list || []);
+  }
 };
 </script>
